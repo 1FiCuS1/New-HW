f450a878aed103f0ffd95de4a9634bd9cbfb1f9b
Мария Алексеева <12ficus12@gmail.com>
2023-12-27 20:29:45 +0400

    Number_2

--- main (1).py
+++ main (1).py
@@ -1,37 +1,49 @@
 class Student:
-  def __init__(self, name, surname, gender):
-      self.name = name
-      self.surname = surname
-      self.gender = gender
-      self.finished_courses = []
-      self.courses_in_progress = []
-      self.grades = {}
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
-  def add_courses(self, course_name):
-      self.finished_courses.append(course_name)   
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+    def rate_hw(self, lecture, course, grade):
+        if isinstance(lecture, Lecture) and  course in self.courses_in_progress and course in lecture.courses_attached:
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[courses] = [grade]
+        else:
+            return 'Ошибка'        
+        
+
 
 
 class Mentor:
-  def __init__(self, name, surname):
-      self.name = name
-      self.surname = surname
-      self.courses_attached = []
-
-  def rate_hw(self, student, course, grade):
-      if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-          if course in student.grades:
-              student.grades[course] += [grade]
-          else:
-              student.grades[course] = [grade]
-      else:
-          return 'Ошибка'
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
 
 class Lecture(Mentor):
-    super().__init__(self, name, surname)
+    super().__init__(name, surname)
+    self.grades = {}
 
 class  Reviewer(Mentor):
-    super().__init__(self, name, surname)
-    
+    super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                 student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 
 
